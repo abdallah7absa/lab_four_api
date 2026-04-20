5241a7f97833b8d1245edf6d6a8d3507fab15cd5
Abdallah Habsa <abdallah7absa@gmail.com>
2026-04-20 02:51:22 +0200

    feat: seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+user1 = User.create!(name: "eyad", email: "eyad@gmail.com")
+user2 = User.create!(name: "habsa", email: "habsa@gmail.com")
+
+Post.create!(title: "post1", content: "sba7 elfol", user: user1)
+Post.create!(title: "post2", content: "msa2 elfol", user: user1)
+Post.create!(title: "post3", content: "sba7 msa2", user: user2)
